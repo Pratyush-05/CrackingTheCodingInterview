fcfd6d2b87a0ae758ecc30a04f7716c6eb63b076
Pratyush-05 <profiesta98@gmail.com>
2022-06-07 09:33:06 +0530

    inital file setup

---
+++ Ch1_Arrays_String/isUniqueA.java
@@ -0,0 +1,14 @@
+class isUniqueA{
+    public static void main(String[] args) {
+        String[] words = {"abcde", "hello", "apple", "kite", "padle"};
+		for (String word : words) {
+			System.out.println(word + ": " + isUniqueChars(word));
+		} 
+    }
+
+    public static boolean isUniqueChars(String str){
+        int[] alphabets = new int[128];
+        
+        return false;
+    }
+}
